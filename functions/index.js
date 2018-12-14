24c033cc5be285af43b714391fe64bfd0858a47f
jdltechworks <jdltechworks@gmail.com>
2018-12-14 16:08:06 +0800

    Adding app_engine feature for cron

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,15 @@ const app = express();
 routes(app);
 
 exports.api = functions.https.onRequest(app);
+
+exports.minute_job = functions.pubsub
+  .topic('project_item_update')
+  .onPublish((message) => {
+    console.log("This job is run every minute!");
+    if (message.data) {
+      const dataString = Buffer.from(message.data, 'base64').toString();
+      console.log(`Message Data: ${dataString}`);
+    }
+
+    return true;
+  });
